7e0f9055f2a8b58f95323dd568c11fc8a44d9682
pavi5 <pavi5@LAPTOP-B44HQDM0>
2023-02-19 22:33:14 +0530

    first commit

---
+++ placement_management_service/src/main/java/placement/PlacementManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package placement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacementManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacementManagementServiceApplication.class, args);
+	}
+
+}
---
+++ placement_management_service/src/test/java/placement/PlacementManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package placement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacementManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
